21e5b96e73059821ae81fea9b0bafefb479ac0fe
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-05-12 02:37:30 +0500

    JSX

---
+++ myapp/src/App.jsx
@@ -0,0 +1,12 @@
+const MyHeading = () => <h1>Heading</h1>;
+
+function App() {
+  return (
+    <div>
+      <MyHeading />
+      <h1>Welcome to React</h1>
+    </div>
+  );
+}
+
+export default App;
