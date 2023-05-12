75a3aeaa3378d6ea0b23e629013a89139db3f33f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-05-13 02:52:00 +0500

    Proj1

--- projone/src/components/Home.jsx
+++ projone/src/components/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 const Home = () => {
   return (
@@ -18,6 +24,63 @@ const Home = () => {
             Lorem ipsum dolor sit amet consectetur adipiscing elit condimentum,
             enim sapien placerat rutrum pulvinar tempus euismod at iaculis,
           </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit condimentum,
+            enim sapien placerat rutrum pulvinar tempus euismod at iaculis,
+            Lorem ipsum dolor sit amet consectetur adipiscing elit condimentum,
+            enim sapien placerat rutrum pulvinar tempus euismod at iaculis,
+            Lorem ipsum dolor sit amet consectetur adipiscing elit condimentum,
+            enim sapien placerat rutrum pulvinar tempus euismod at iaculis,
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>YouTube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
         </div>
       </div>
     </>
