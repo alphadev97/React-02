2f2acd3ef924755c03f0984cb3af04c2dd5cc610
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-05-19 01:39:10 +0500

    update

--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -1,46 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Coin from "./Product";
-import axios from "axios";
-import Loader from "./Loader";
+import React from "react";
 
 const Home = () => {
-  const [coins, setCoins] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchAllCoins = async () => {
-      try {
-        const { data } = await axios.get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=pkr&per_page=20"
-        );
-        console.log(data);
-
-        setCoins(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchAllCoins();
-  }, []);
-
-  return (
-    <div className="home">
-      {loading ? (
-        <Loader />
-      ) : (
-        coins.map((i) => (
-          <Coin
-            name={i.name}
-            symbol={i.symbol}
-            key={i.id}
-            imgSrc={i.image}
-            price={i.current_price}
-          />
-        ))
-      )}
-    </div>
-  );
+  return <div className="home">Home</div>;
 };
 
 export default Home;
